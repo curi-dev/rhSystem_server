363555880d143addc4f0b04d8383a8980b287470
curi <shopper.tiago@gmail.com>
2023-09-04 19:55:18 -0300

    deploy config

--- app/infrastructure/database/index.go
+++ app/infrastructure/database/index.go
@@ -1,7 +1,6 @@
 package database
 
 import (
-	"fmt"
 	"log"
 	"os"
 
@@ -29,13 +28,14 @@ func Init() {
 
 		godotenv.Load(".env")
 
-		user := os.Getenv("DATABASE_USER")
-		password := os.Getenv("DATABASE_PASSWORD")
-		host := os.Getenv("DATABASE_HOST")
-		database := os.Getenv("DATABASE_NAME")
-		sslMode := os.Getenv("SSL_MODE") // use "verify-full" in prod
+		// user := os.Getenv("DATABASE_USER")
+		// password := os.Getenv("DATABASE_PASSWORD")
+		// host := os.Getenv("DATABASE_HOST")
+		// database := os.Getenv("DATABASE_NAME")
+		// sslMode := os.Getenv("SSL_MODE") // use "verify-full" in prod
 
-		dsn := fmt.Sprintf("postgres://%s:%s@%s/%s?sslmode=%s", user, password, host, database, sslMode)
+		// dsn := fmt.Sprintf("postgres://%s:%s@%s/%s?sslmode=%s", user, password, host, database, sslMode)
+		dsn := os.Getenv("DATABASE_URL")
 		driver := "postgres"
 
 		db, err = sql.Open(driver, dsn)
@@ -44,6 +44,10 @@ func Init() {
 			log.Fatal(err)
 		}
 
-		db.Ping() // what do this function really does
+		err := db.Ping() // what do this function really does
+
+		if err != nil {
+			panic("[database]: Ocorreu um erro no servidor")
+		}
 	})
 }
