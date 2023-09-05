99e140d982ce2c17195081d3f6ce2697c7e8c76a
curi <shopper.tiago@gmail.com>
2023-09-04 23:39:01 -0300

    enable *

--- app/application/helpers/enable_cors.go
+++ app/application/helpers/enable_cors.go
@@ -3,6 +3,6 @@ package helpers
 import "net/http"
 
 func EnableCors(w *http.ResponseWriter) {
-	(*w).Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
+	(*w).Header().Set("Access-Control-Allow-Origin", "*")
 	(*w).Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
 }
